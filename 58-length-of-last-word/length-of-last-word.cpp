f4e1cbc8b53e41c17ccef3956ed315f144ccd356
Ujjawal-Pandey <ujjawalpandey2005@gmail.com>
2026-08-12 23:48:37 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (8.83%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        long i = s.length() - 1;
+        long iLength = 0;
+
+        while (s[i] == ' ') i--;
+
+        while (i >= 0 && s[i] != ' ') {
+            iLength++;
+            i--;
+        }
+
+        return iLength;
+    }
+};
