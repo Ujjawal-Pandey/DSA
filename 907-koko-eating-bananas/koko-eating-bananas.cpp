82fef6bbbe49abfa00e2efc1eabfbe82e515422c
Ujjawal-Pandey <ujjawalpandey2005@gmail.com>
2026-08-26 04:04:44 +0530

    Time: 12 ms (26.13%) | Memory: 23.1 MB (23.07%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <algorithm>
+long long cal(vector<int>& piles, int y){
+long long sum = 0;
+    for(int i = 0 ; i<piles.size(); i++){
+        double x = (double)piles[i]/y;
+        int a = ceil(x);
+        sum= sum+a;
+        //sum += ceil((double)piles[i]/(double)y);
+    }
+    return sum;
+}
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int sum = 0 ;
+        int high = *max_element(piles.begin(), piles.end());
+        int low= 1;
+        while(low<high){
+            int mid= (low+high)/2;
+            if(cal(piles,mid)<=h){
+                high = mid;
+            }
+            else{
+                low =mid+1;
+            }
+
+        }
+
+        return low;
+        
+    }
+};
