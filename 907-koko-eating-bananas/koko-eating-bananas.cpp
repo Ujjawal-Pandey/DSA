f115df24cada6f0d233bd59ee2a2ba8d194b032d
Ujjawal-Pandey <ujjawalpandey2005@gmail.com>
2026-08-26 04:05:35 +0530

    Time: 15 ms (17.22%) | Memory: 23 MB (57.56%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -3,10 +3,10 @@
 long long cal(vector<int>& piles, int y){
 long long sum = 0;
     for(int i = 0 ; i<piles.size(); i++){
-        double x = (double)piles[i]/y;
-        int a = ceil(x);
-        sum= sum+a;
-        //sum += ceil((double)piles[i]/(double)y);
+        // double x = (double)piles[i]/y;
+        // int a = ceil(x);
+        // sum= sum+a;
+        sum += ceil((double)piles[i]/(double)y);
     }
     return sum;
 }
@@ -24,9 +24,7 @@ public:
             else{
                 low =mid+1;
             }
-
         }
-
         return low;
         
     }
